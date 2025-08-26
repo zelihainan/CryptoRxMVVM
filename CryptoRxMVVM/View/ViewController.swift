16171196565e3b7bc2281c005181fd86c809b090
Zeliha İnan <zelihainann@gmail.com>
2025-08-27 00:20:56 +0300

    Constraints updated

--- CryptoRxMVVM/View/ViewController.swift
+++ CryptoRxMVVM/View/ViewController.swift
@@ -9,23 +9,30 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var indicatorView: UIActivityIndicatorView!
     
+    @IBOutlet weak var searchBar: UISearchBar!
     
     let cryptoVM = CryptoViewModel()
     let disposeBag = DisposeBag()
     var cryptoList = [Crypto]()
+    
+    private var filteredList = [Crypto]()
+    private var isSearching = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         
+        searchBar.placeholder = "Search"
+        
         setUpBindings()
+        bindSearchBar()
         cryptoVM.requestData()
     }
     
@@ -49,21 +56,57 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             .observe(on: MainScheduler.asyncInstance)
             .subscribe { cryptoArray in
                 self.cryptoList = cryptoArray
+                
+                if !self.isSearching {
+                    self.filteredList = self.cryptoList
+                }
+                
                 self.tableView.reloadData()
             }
             .disposed(by: disposeBag)
     }
     
+    private func bindSearchBar() {
+        searchBar.rx.text.orEmpty
+            .distinctUntilChanged()
+            .debounce(.milliseconds(200), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [weak self] query in
+                guard let self = self else { return }
+
+                let q = query
+                    .folding(options: .diacriticInsensitive, locale: .current)
+                    .lowercased()
+
+                if q.isEmpty {
+                    self.isSearching = false
+                    self.filteredList = self.cryptoList
+                } else {
+                    self.isSearching = true
+                    self.filteredList = self.cryptoList.filter {
+                        // currency’de veya price string’inde arama
+                        $0.currency.folding(options: .diacriticInsensitive, locale: .current)
+                            .lowercased().contains(q)
+                        ||
+                        $0.price.lowercased().contains(q)
+                    }
+                }
+
+                self.tableView.reloadData()
+            })
+            .disposed(by: disposeBag)
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cryptoList.count
+        return filteredList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         var content = cell.defaultContentConfiguration()
-        content.text = cryptoList[indexPath.row].currency
-        content.secondaryText = cryptoList[indexPath.row].price
+        let item = filteredList[indexPath.row]
+        content.text = item.currency
+        content.secondaryText = item.price
         cell.contentConfiguration = content
         return cell
     }
